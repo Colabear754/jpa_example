6c4150a599f27707f9a16422eac3eafaa4a73dd3
Colabear754 <jky2406@gmail.com>
2023-03-03 22:06:12 +0900

    Initial commit

---
+++ src/main/kotlin/com/colabear754/jpa_example/JpaExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.colabear754.jpa_example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpaExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<JpaExampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/colabear754/jpa_example/JpaExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.colabear754.jpa_example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpaExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
