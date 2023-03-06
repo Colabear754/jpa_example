82cbe9eaf26ea76958642732520466c21f1c372e
Colabear754 <jky2406@gmail.com>
2023-03-06 23:25:53 +0900

    멤버추가 request 타입 변경

--- src/main/kotlin/com/colabear754/jpa_example/controllers/member/MemberController.kt
+++ src/main/kotlin/com/colabear754/jpa_example/controllers/member/MemberController.kt
@@ -3,6 +3,7 @@ package com.colabear754.jpa_example.controllers.member
 import com.colabear754.jpa_example.entity.member.Member
 import com.colabear754.jpa_example.services.member.MemberService
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,5 +11,5 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping
 class MemberController(private val memberService: MemberService) {
     @PostMapping("/member")
-    fun insertMember(member: Member) = memberService.insertMember(member)
+    fun insertMember(@RequestBody member: Member) = memberService.insertMember(member)
 }
