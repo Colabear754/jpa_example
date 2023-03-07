eef4aad35346f663c4d198958cb5f9b6ec4ed293
Colabear754 <jky2406@gmail.com>
2023-03-07 13:06:28 +0900

    멤버수정

--- src/main/kotlin/com/colabear754/jpa_example/controllers/member/MemberController.kt
+++ src/main/kotlin/com/colabear754/jpa_example/controllers/member/MemberController.kt
@@ -2,14 +2,14 @@ package com.colabear754.jpa_example.controllers.member
 
 import com.colabear754.jpa_example.entity.member.Member
 import com.colabear754.jpa_example.services.member.MemberService
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping
+@RequestMapping("/member")
 class MemberController(private val memberService: MemberService) {
-    @PostMapping("/member")
+    @PostMapping("/regist")
     fun insertMember(@RequestBody member: Member) = memberService.insertMember(member)
+
+    @PutMapping("/update/{id}")
+    fun updateMember(@PathVariable id: Long, @RequestBody newMember: Member) = memberService.updateMember(id, newMember)
 }
