7b76f68bbfbde01ef0348b4c98dedb42e8bb4110
Colabear754 <jky2406@gmail.com>
2023-03-12 17:29:33 +0900

    패키지 변경

--- src/main/kotlin/com/colabear754/jpa_example/repository/member/MemberRepository.kt
+++ src/main/kotlin/com/colabear754/jpa_example/repository/member/MemberRepository.kt
@@ -1,5 +1,6 @@
-package com.colabear754.jpa_example.entity.member
+package com.colabear754.jpa_example.repository.member
 
+import com.colabear754.jpa_example.entity.member.Member
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface MemberRepository : JpaRepository<Member, Long> {
