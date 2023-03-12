a1dea64b0c96a62e7db856181b576374c807ff98
Colabear754 <jky2406@gmail.com>
2023-03-12 17:30:04 +0900

    멤버 클래스에 새로운 프로퍼티 추가

--- src/main/kotlin/com/colabear754/jpa_example/entity/member/Member.kt
+++ src/main/kotlin/com/colabear754/jpa_example/entity/member/Member.kt
@@ -1,7 +1,9 @@
 package com.colabear754.jpa_example.entity.member
 
+import com.colabear754.jpa_example.common.UserRole
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.persistence.*
+import java.time.LocalDate
 
 @Entity
 class Member(
@@ -13,10 +15,22 @@ class Member(
     @Schema(description = "이름", example = "홍길동")
     var name: String,
     @Schema(description = "나이", example = "20")
-    var age: Int
+    var age: Int?,
+    @Schema(description = "권한", example = "USER")
+    @Enumerated(EnumType.STRING)
+    var role: UserRole,
+    @Schema(description = "비고", example = "비고")
+    var remark: String?,
+    @Schema(hidden = true)
+    val createDate: LocalDate = LocalDate.now(),
+    @Schema(hidden = true)
+    var updateDate: LocalDate = LocalDate.now()
 ) {
     fun update(newMember: Member) {
         this.name = newMember.name
         this.age = newMember.age
+        this.role = newMember.role
+        this.remark = newMember.remark
+        this.updateDate = LocalDate.now()
     }
 }
