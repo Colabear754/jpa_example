14e8252fe56a03d3a9a1286cbbbc523102d01630
Colabear754 <jky2406@gmail.com>
2023-03-30 16:00:41 +0900

    엔티티에 필요 없는 스웨거 설명 제거 및 BaseEntity 상속
    스웨거 설명은 DTO에 추가하는게 좋아 보임

--- src/main/kotlin/com/colabear754/jpa_example/entities/member/Member.kt
+++ src/main/kotlin/com/colabear754/jpa_example/entities/member/Member.kt
@@ -1,30 +1,28 @@
 package com.colabear754.jpa_example.entities.member
 
+import com.colabear754.jpa_example.entities.BaseEntity
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.persistence.*
 import java.util.UUID
 
 @Entity
 class Member(
+    @Column(nullable = false)
+    var name: String,
+    var age: Int?,
+    @Column(nullable = false)
+    var zipCode: String,
+    @Column(nullable = false)
+    var address: String?,
+    @Column(nullable = false)
+    var phoneNumber: String,
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Schema(hidden = true)
-    val id: UUID?,
-    @Schema(description = "이름", example = "홍길동")
-    @Column(nullable = false)
-    var name: String,
-    @Schema(description = "나이", example = "20")
-    var age: Int?,
-    @Schema(description = "우편번호", example = "12345")
-    @Column(nullable = false)
-    var zipCode: String,
-    @Schema(description = "주소", example = "서울시 강남구")
-    @Column(nullable = false)
-    var address: String?,
-    @Schema(description = "전화번호", example = "010-1234-5678")
-    @Column(nullable = false)
-    var phoneNumber: String
-) {
+    val id: UUID? = null,
+    createdBy: String? = null,
+    lastModifiedBy: String? = null
+) : BaseEntity(createdBy = createdBy, lastModifiedBy = lastModifiedBy) {
     fun update(newMember: Member) {
         this.name = newMember.name
         this.age = newMember.age
