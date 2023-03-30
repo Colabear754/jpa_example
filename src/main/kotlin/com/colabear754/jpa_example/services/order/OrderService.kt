7d0f25891bef27c4709ddc019bef94d304650ddb
Colabear754 <jky2406@gmail.com>
2023-03-30 15:44:42 +0900

    주문조회, 주문취소 메소드 추가

--- src/main/kotlin/com/colabear754/jpa_example/services/order/OrderService.kt
+++ src/main/kotlin/com/colabear754/jpa_example/services/order/OrderService.kt
@@ -1,15 +1,27 @@
 package com.colabear754.jpa_example.services.order
 
+import com.colabear754.jpa_example.entities.member.Member
 import com.colabear754.jpa_example.entities.member.order.Order
 import com.colabear754.jpa_example.repositories.member.order.OrderRepository
+import com.colabear754.jpa_example.util.findByIdOrThrow
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.util.UUID
 
 @Service
 class OrderService(private val orderRepository: OrderRepository) {
+    @Transactional(readOnly = true)
+    fun getOrders(member: Member): List<Order> = orderRepository.findByMember(member)
+
     @Transactional
     fun newOrder(order: Order) = orderRepository.save(order)
 
     @Transactional
     fun newOrders(orders: List<Order>): List<Order> = orderRepository.saveAll(orders)
+
+    @Transactional
+    fun cancelOrder(id: UUID) {
+        val order = orderRepository.findByIdOrThrow(id, "존재하지 않는 주문번호입니다.")
+        order.cancel()
+    }
 }
