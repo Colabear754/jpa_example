63fb0e83097c15fedfcebb8b7761011feed361f6
Colabear754 <jky2406@gmail.com>
2023-03-30 15:45:06 +0900

    주문조회 파라미터를 id가 아닌 Member 객체로 변경

--- src/main/kotlin/com/colabear754/jpa_example/repositories/member/order/OrderRepository.kt
+++ src/main/kotlin/com/colabear754/jpa_example/repositories/member/order/OrderRepository.kt
@@ -1,9 +1,10 @@
 package com.colabear754.jpa_example.repositories.member.order
 
+import com.colabear754.jpa_example.entities.member.Member
 import com.colabear754.jpa_example.entities.member.order.Order
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.UUID
+import java.util.*
 
 interface OrderRepository : JpaRepository<Order, UUID> {
-    fun findByMemberId(id: UUID): List<Order>
+    fun findByMember(member: Member): List<Order>
 }
