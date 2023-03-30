adddab879dd178c44c1cfc344f285b0b3bf15c07
Colabear754 <jky2406@gmail.com>
2023-03-30 19:36:47 +0900

    확장함수 모음과 예외 유틸 파일 분리

--- src/main/kotlin/com/colabear754/jpa_example/util/ExceptionUtils.kt
+++ src/main/kotlin/com/colabear754/jpa_example/util/ExceptionUtils.kt
@@ -1,10 +1,3 @@
 package com.colabear754.jpa_example.util
 
-import org.springframework.data.repository.CrudRepository
-import org.springframework.data.repository.findByIdOrNull
-
-fun fail(message: String): Nothing {
-    throw NoSuchElementException(message)
-}
-
-inline fun <reified T, ID> CrudRepository<T, ID>.findByIdOrThrow(id: ID, message: String = "해당 ID의 데이터가 존재하지 않습니다."): T = findByIdOrNull(id) ?: fail(message)
+fun fail(message: String): Nothing = throw NoSuchElementException(message)
---
+++ src/main/kotlin/com/colabear754/jpa_example/util/RepositoryExtensions.kt
@@ -0,0 +1,6 @@
+package com.colabear754.jpa_example.util
+
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.findByIdOrNull
+
+inline fun <reified T, ID> CrudRepository<T, ID>.findByIdOrThrow(id: ID, message: String = "해당 ID의 데이터가 존재하지 않습니다."): T = findByIdOrNull(id) ?: fail(message)
