1dd7bc7e4c18cf203def35bc12bbe77ae7df00ed
Colabear754 <jky2406@gmail.com>
2023-04-06 13:34:43 +0900

    불필요한 프로퍼티 제거

--- src/main/kotlin/com/colabear754/jpa_example/services/member/MemberService.kt
+++ src/main/kotlin/com/colabear754/jpa_example/services/member/MemberService.kt
@@ -2,14 +2,13 @@ package com.colabear754.jpa_example.services.member
 
 import com.colabear754.jpa_example.entities.member.Member
 import com.colabear754.jpa_example.repositories.member.MemberRepository
-import com.colabear754.jpa_example.repositories.member.order.OrderRepository
 import com.colabear754.jpa_example.util.findByIdOrThrow
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.util.UUID
+import java.util.*
 
 @Service
-class MemberService(private val memberRepository: MemberRepository, private val orderRepository: OrderRepository) {
+class MemberService(private val memberRepository: MemberRepository) {
     @Transactional(readOnly = true)
     fun getMember(id: UUID): Member = memberRepository.findByIdOrThrow(id, "존재하지 않는 회원입니다.")
 
